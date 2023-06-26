5acc8a3d10a5a46edf8a50d424309975b9b6850d
yan9a <yan9aye@gmail.com>
2023-06-27 01:42:20 +1200

    Update sha1_c

--- sha1_c/main.cpp
+++ sha1_c/main.cpp
@@ -4,11 +4,12 @@
 #include "sha1.h"
 int main(int, char**) {
     //std::string message = "12345678";
-    SHA1_CTX sha; uint8_t results[20]; char *buf; int n;
-    buf = "12345678"; n = strlen(buf); SHA1Init(&sha); SHA1Update(&sha, (uint8_t *)buf, n); SHA1Final(results, &sha);
+    SHA1_CTX sha; uint8_t results[20]; int n;
+    const char *buf = "12345678"; n = strlen(buf); SHA1Init(&sha); SHA1Update(&sha, (uint8_t *)buf, n); SHA1Final(results, &sha);
     /* Print the digest as one long hex value */ printf("0x"); 
     for (n = 0; n < 20; n++)
     printf("%02x", results[n]);
     putchar('\n');
+    // result = 0x7c222fb2927d828af22f592134e8932480637c0d
     return 0;
 }
